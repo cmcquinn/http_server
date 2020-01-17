402d0555df4816c216a511f75bba7f328f8e46cf
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-01-17 12:33:45 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2020-01-16
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
+#include "http.h"
+#include <stdio.h>
+
+void usage() {
+
+}
+
+int main(int argc, char *argv[]) {
+    printf("http server\n");
+}

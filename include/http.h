8d13f916e5011f41bc1b0ad9a0cc5df3f617974e
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-01-30 15:48:01 -0700

    http: Validate requests

--- include/http.h
+++ include/http.h
@@ -4,7 +4,18 @@
  * @brief http parsing module.
  * @version 0.1
  * @date 2020-01-28
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
+
+#include <stdbool.h>
+
+/**
+ * @brief Check if a character buffer contains a valid http request.
+ *
+ * @param buf Pointer to a char buf.
+ * @return true Buffer contains a valid http request.
+ * @return false Buffer does not contain a valid http request.
+ */
+bool http_contains_valid_request(const char *buf);
--- src/http.c
+++ src/http.c
@@ -4,7 +4,40 @@
  * @brief http parsing module
  * @version 0.1
  * @date 2020-01-28
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
+
+#include "http.h"
+#include <string.h>
+
+/**
+ * @brief Check if a character buffer contains a valid http request.
+ *
+ * @param buf Pointer to a char buf.
+ * @return true Buffer contains a valid http request.
+ * @return false Buffer does not contain a valid http request.
+ */
+bool http_contains_valid_request(const char *buf) {
+    const char *ptr;
+
+    // find GET
+    if ((ptr = strstr(buf, "GET ")) == NULL)
+        return false;
+
+    // find HTTP/1.1
+    if ((ptr = strstr(ptr, "HTTP/1.1\r\n")) == NULL)
+        return false;
+
+    // find Host field
+    if ((ptr = strstr(ptr, "Host: ")) == NULL)
+        return false;
+
+    // find end of host field line
+    if ((ptr = strstr(ptr, "\r\n")) == NULL)
+        return false;
+
+    // at the point we have a minimal HTTP/1.1 message
+    return true;
+}

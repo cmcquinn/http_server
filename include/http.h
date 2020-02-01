9574eaca3dfc91944ed854dabcb2a5089afc280b
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-02-01 11:06:57 -0700

    Added http_get_method function

--- include/http.h
+++ include/http.h
@@ -1,7 +1,7 @@
 /**
  * @file http.h
  * @author Cameron McQuinn (cameron.mcquinn@gmail.com)
- * @brief http parsing module.
+ * @brief HTTP parsing module.
  * @version 0.1
  * @date 2020-01-28
  *
@@ -9,13 +9,62 @@
  *
  */
 
+#ifndef HTTP_H
+#define HTTP_H
+
 #include <stdbool.h>
 
+#define HTTP_ERROR   -1 //!< Return code indicating error.
+#define HTTP_SUCCESS 0  //!< Return code indicating success.
+
+//! Enum representing the different possible HTTP methods.
+typedef enum http_method {
+    HTTP_GET,         //!< HTTP GET method
+    HTTP_HEAD,        //!< HTTP HEAD method
+    HTTP_POST,        //!< HTTP POST method
+    HTTP_PUT,         //!< HTTP PUT method
+    HTTP_DELETE,      //!< HTTP DELETE method
+    HTTP_CONNECT,     //!< HTTP CONNECT method
+    HTTP_OPTIONS,     //!< HTTP OPTIONS method
+    HTTP_TRACE,       //!< HTTP TRACE method
+    HTTP_PATCH,       //!< HTTP PATCH method
+    HTTP_METHOD_NONE, //!< No HTTP method found
+} http_method_t;
+
+//! Struct represending an HTTP message.
+struct http_message {
+    http_method_t method; /**< HTTP method specified in the message. */
+    const char *resource; /**< Path to the requested resource. */
+    const char *header;   /**< HTTP header fields. */
+    const char *body;     /**< Message body */
+};
+
 /**
- * @brief Check if a character buffer contains a valid http request.
+ * @brief Check if a character buffer contains a valid HTTP request.
  *
  * @param buf Pointer to a char buf.
- * @return true Buffer contains a valid http request.
- * @return false Buffer does not contain a valid http request.
+ * @return true Buffer contains a valid HTTP request.
+ * @return false Buffer does not contain a valid HTTP request.
  */
-bool http_contains_valid_request(const char *buf);
+bool http_contains_valid_message(const char *buf);
+
+/**
+ * @brief Extract an HTTP message from a char buf and put it into a struct http_message.
+ *
+ * @param buf Char buf containing a valid HTTP message.
+ * @param message Pointer to a struct http_message to be filled with the contents of the message.
+ * @return int HTTP_SUCCESS if \p buf contains a valid HTTP message, HTTP_ERROR otherwise.
+ */
+int http_extract_message(const char *buf, struct http_message *message);
+
+/**
+ * @brief Format a response to an HTTP message containing a request.
+ *
+ * @param message Pointer to an http_message struct containing a request.
+ * @param response Pointer to an http_message struct which will be filled with the response to the
+ * HTTP message represented by \p message.
+ * @return int HTTP_SUCCESS if \p message contains a valid request, HTTP_ERROR otherwise.
+ */
+int http_prepare_response(struct http_message *message, struct http_message *response);
+
+#endif // HTTP_H
--- src/http.c
+++ src/http.c
@@ -1,7 +1,7 @@
 /**
  * @file http.c
  * @author Cameron McQuinn (cameron.mcquinn@gmail.com)
- * @brief http parsing module
+ * @brief HTTP parsing module
  * @version 0.1
  * @date 2020-01-28
  *
@@ -12,14 +12,24 @@
 #include "http.h"
 #include <string.h>
 
+#define HTTP_GET_STR     "GET"     //!< HTTP GET method
+#define HTTP_HEAD_STR    "HEAD"    //!< HTTP HEAD method
+#define HTTP_POST_STR    "POST"    //!< HTTP POST method
+#define HTTP_PUT_STR     "PUT"     //!< HTTP PUT method
+#define HTTP_DELETE_STR  "DELETE " //!< HTTP DELETE method
+#define HTTP_CONNECT_STR "CONNECT" //!< HTTP CONNECT method
+#define HTTP_OPTIONS_STR "OPTIONS" //!< HTTP OPTIONS method
+#define HTTP_TRACE_STR   "TRACE"   //!< HTTP TRACE method
+#define HTTP_PATCH_STR   "PATCH"   //!< HTTP PATCH method
+
 /**
- * @brief Check if a character buffer contains a valid http request.
+ * @brief Check if a character buffer contains a valid HTTP request.
  *
  * @param buf Pointer to a char buf.
- * @return true Buffer contains a valid http request.
- * @return false Buffer does not contain a valid http request.
+ * @return true Buffer contains a valid HTTP request.
+ * @return false Buffer does not contain a valid HTTP request.
  */
-bool http_contains_valid_request(const char *buf) {
+bool http_contains_valid_message(const char *buf) {
     const char *ptr;
 
     // find GET
@@ -41,3 +51,60 @@ bool http_contains_valid_request(const char *buf) {
     // at the point we have a minimal HTTP/1.1 message
     return true;
 }
+
+/**
+ * @brief Extract the HTTP method from a char buf containing an HTTP message and put it into a
+ * struct http_message.
+ *
+ * @param buf Char buf containing a valid HTTP message.
+ * @param message Pointer to a struct http_message to be filled with the contents of the message.
+ * @return const char* Pointer to next character after the end of the HTTP method in \p buf if an
+ * HTTP method is found, NULL otherwise.
+ */
+static const char *http_get_method(const char *buf, struct http_message *message) {
+    const char *pch = NULL;
+    if ((pch = strstr(buf, HTTP_GET_STR)) != NULL) {
+        message->method = HTTP_GET;
+    } else if ((pch = strstr(buf, HTTP_HEAD_STR)) != NULL) {
+        message->method = HTTP_HEAD;
+    } else if ((pch = strstr(buf, HTTP_POST_STR)) != NULL) {
+        message->method = HTTP_POST;
+    } else if ((pch = strstr(buf, HTTP_PUT_STR)) != NULL) {
+        message->method = HTTP_PUT;
+    } else if ((pch = strstr(buf, HTTP_DELETE_STR)) != NULL) {
+        message->method = HTTP_DELETE;
+    } else if ((pch = strstr(buf, HTTP_CONNECT_STR)) != NULL) {
+        message->method = HTTP_CONNECT;
+    } else if ((pch = strstr(buf, HTTP_OPTIONS_STR)) != NULL) {
+        message->method = HTTP_OPTIONS;
+    } else if ((pch = strstr(buf, HTTP_TRACE_STR)) != NULL) {
+        message->method = HTTP_TRACE;
+    } else if ((pch = strstr(buf, HTTP_PATCH_STR)) != NULL) {
+        message->method = HTTP_PATCH;
+    } else {
+        message->method = HTTP_METHOD_NONE;
+    }
+    return strstr(pch, " "); // find space after method
+}
+
+/**
+ * @brief Extract an HTTP message from a char buf and put it into a struct http_message.
+ *
+ * @param buf Char buf containing a valid HTTP message.
+ * @param message Pointer to a struct http_message to be filled with the contents of the message.
+ * @return int HTTP_SUCCESS if \p buf contains a valid HTTP message, HTTP_ERROR otherwise.
+ */
+int http_extract_message(const char *buf, struct http_message *message) {
+    const char *pch;
+    pch = http_get_method(buf, message);
+}
+
+/**
+ * @brief Format a response to an HTTP message containing a request.
+ *
+ * @param message Pointer to an http_message struct containing a request.
+ * @param response Pointer to an http_message struct which will be filled with the response to the
+ * HTTP message represented by \p message.
+ * @return int HTTP_SUCCESS if \p message contains a valid request, HTTP_ERROR otherwise.
+ */
+int http_prepare_response(struct http_message *message, struct http_message *response);
